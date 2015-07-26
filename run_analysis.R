d618286788e3c2ceb896fa42b3bf12b2a8d4aa5d
Michael Brown <mbrown@incipire.com>
2015-07-26 18:15:06 -0500

    Step 4

---
+++ run_analysis.R
@@ -0,0 +1,39 @@
+#Read Test and Train data in
+con<-unz("Archive.zip","test/X_test.txt")
+testData<-read.table(con)
+con<-unz("Archive.zip","train/X_train.txt")
+trainData<-read.table(con)
+combinedData<-rbind(testData,trainData)
+
+#Read column names in
+con<-unz("Archive.zip", "features.txt")
+features<-read.table(con)
+
+#Map column names to test data
+colnames(combinedData)<-features[,2]
+
+
+#Pull out std and mean columns into data frame
+stdMeanColumns<-grep("std|mean[^meanFreq]",features[,2], fixed = FALSE)
+combinedData<-combinedData[,stdMeanColumns]
+
+#Create columns for subject and activity
+con<-unz("Archive.zip", "test/y_test.txt")
+testActivities <- read.table(con)
+con<-unz("Archive.zip", "train/y_train.txt")
+trainActivities <- read.table(con)
+combinedActivities <-rbind(testActivities,trainActivities)
+con<-unz("Archive.zip","test/subject_test.txt")
+testSubjects<-read.table(con)
+con<-unz("Archive.zip","train/subject_train.txt")
+trainSubjects<-read.table(con)
+combinedSubjects<-rbind(testSubjects,trainSubjects)
+combinedSubjects <- cbind(combinedSubjects,combinedActivities)
+colnames(combinedSubjects)<-c("Subject", "Activity")
+
+combinedData<-cbind(combinedSubjects,combinedData)
+
+con<-unz("Archive.zip", "activity_labels.txt")
+labels<-read.table(con)
+colnames(labels)<-c("Id","Activity")
+mergedData=merge(labels,combinedData, by.x="Id", by.y="Activity")
